27bfbc8073e2c7a789ea845ce0f5b53fcece2341
lalo <eduardocarhue@gmail.com>
2019-03-15 14:24:50 -0600

    Create First Version of Models

--- electro/electro_app/models.py
+++ electro/electro_app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    price_old = models.FloatField()
+    price_new = models.FloatField()
+
+class  Description(models.Model):
+    product = models.ForeignKey(Product, 
+            on_delete=models.CASCADE)
+
+class Extra_Info(models.Model):
+    product = models.ForeignKey(Product, 
+            on_delete=models.CASCADE)
+    info = models.CharField(max_length=256)
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, 
+            on_delete=models.CASCADE)
+    stars = models.IntegerField()
+
+class Comments(models.Model):
+    product = models.ForeignKey(Product, 
+            on_delete=models.CASCADE)
+    stars = models.IntegerField()
+    date = models.DateTimeField()
+    comment = models.CharField(max_length=256)
+
+
